afddc1151e4a8dfad8df33849902c97a50fa4c03
Perinparasa N. <UT01154@unicomtic.com>
2024-10-11 10:13:15 +0530

    Q1

--- obj/Debug/net8.0/FitnessProgramManagementSystem_V1.AssemblyInfo.cs
+++ obj/Debug/net8.0/FitnessProgramManagementSystem_V1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FitnessProgramManagementSystem_V1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e0bd8d7d42676a3329eee7c0c4ca476bd024718")]
 [assembly: System.Reflection.AssemblyProductAttribute("FitnessProgramManagementSystem_V1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FitnessProgramManagementSystem_V1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
